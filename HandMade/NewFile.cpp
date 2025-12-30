695415840f3e9038a9757017280020712d1fc12b
Sada <sadasiva.maddali123@gmail.com>
2025-12-30 11:25:39 +0530

    Intro to BitMap Info...

--- HandMade/NewFile.cpp
+++ HandMade/NewFile.cpp
@@ -1,14 +1,59 @@
 #include <windows.h>
 
 #define internal static
+#define global_var static
 
+
+
+global_var bool running;
+global_var void* bitmapMemory;
+global_var BITMAPINFO bitmapinfo;
+global_var HBITMAP bitmapHandle;
+global_var HDC bitmapDeviceContext;
 
 //DIB - Device independent Bitmap
 // This function Creates a new bitmap when ever window size changes
+/** While creating a new bitmap
+Either you can first free the old buffer and create a new one or
+You can create a new buffer then free the old one if this fails we can
+fall back to first one
+**/
 internal void
 ResizeDIBSection(int width, int height)
 {
+	if(bitmapHandle)
+	{
+		DeleteObject(bitmapHandle);
+	}
+	if(!bitmapDeviceContext)
+	{
+		bitmapDeviceContext = CreateCompatibleDC(0);
+	}
 	
+	
+	bitmapinfo.bmiHeader.biSize = sizeof(bitmapinfo.bmiHeader);
+	bitmapinfo.bmiHeader.biWidth = width;
+	bitmapinfo.bmiHeader.biHeight = height;
+	bitmapinfo.bmiHeader.biPlanes = 1;
+	bitmapinfo.bmiHeader.biBitCount = 32;
+	bitmapinfo.bmiHeader.biCompression = BI_RGB;
+	
+	bitmapHandle = CreateDIBSection(
+			0 , &bitmapinfo, DIB_RGB_COLORS,
+			&bitmapMemory, 0,0);
+	
+}
+
+internal void 
+updateWindow(HDC deviceContext, int x, int y, int width, int height)
+{
+	StretchDIBits(
+		deviceContext,
+		x, y, width, height,
+		x, y, width, height,
+		bitmapMemory,
+		&bitmapinfo,
+		DIB_RGB_COLORS, SRCCOPY);
 }
 
 LRESULT MainCallBack(
@@ -27,9 +72,9 @@ LRESULT MainCallBack(
 			RECT clientRect;
 			// This gives the actual part where you can draw into - 
 			// Where as GetWindowRect gives total rect including scrollbar,buttons,...
-			BOOL GetClientRect(hWnd, &clientRect);
+			GetClientRect(windowHandle, &clientRect);
 			int width = clientRect.right - clientRect.left;
-			int height = clientRect.top - clientRect.bottom;
+			int height = clientRect.bottom - clientRect.top;
 			// When window size changes we want to redraw our things based on the new window size
 			ResizeDIBSection(width, height);
 			OutputDebugStringA("WM_SIZE\n");
@@ -38,7 +83,7 @@ LRESULT MainCallBack(
 		// This message is posted when window is closed (X button, alt+f4,...)
 		case WM_CLOSE:
 		{
-			PostQuitMessage(0);
+			running = false;
 			OutputDebugStringA("WM_CLOSE\n");
 			break;
 		}
@@ -55,28 +100,23 @@ LRESULT MainCallBack(
 		}
 		case WM_PAINT:
 		{
-			PAINTSTRUCT Paint;
-			HDC DeviceContext = BeginPaint(windowHandle, &Paint);
-			RECT rect = Paint.rcPaint;
+			PAINTSTRUCT paint;
+			HDC deviceContext = BeginPaint(windowHandle, &paint);
+			RECT rect = paint.rcPaint;
+			int x = rect.left;
+			int y = rect.top;
+			int width = rect.right - rect.left;
+			int height = rect.bottom - rect.top;
 			static DWORD color = BLACKNESS;
-			PatBlt(DeviceContext, rect.left, rect.top, rect.right-rect.left, rect.top-rect.bottom, color);
-			if(color == BLACKNESS)
-			{
-				color = WHITENESS;
-			}
-			else 
-			{
-				color = BLACKNESS;
-			}
-			EndPaint(windowHandle, &Paint);
+			updateWindow(deviceContext, x, y, width, height);
+			EndPaint(windowHandle, &paint);
 
 			break;
 		}
 		default:
 		{
 			OutputDebugStringA("Default\n");
-			result = DefWindowProc( windowHandle, message, wParam, lParam);
-			break;
+			return DefWindowProc( windowHandle, message, wParam, lParam);
 		}
 	}
 	return result;
@@ -89,17 +129,17 @@ WinMain(
   LPSTR     lpCmdLine,
   int       nShowCmd)
 {
-	WNDCLASS windowClass = {};
+	WNDCLASSA windowClass = {};
 	windowClass.style = CS_OWNDC|CS_HREDRAW|CS_HREDRAW ; 
 	windowClass.lpfnWndProc = MainCallBack; 
 	windowClass.hInstance = hInstance /*GetModuleHandle(0)*/; 
 	//windowClass.hIcon;  
 	windowClass.lpszClassName = "HandMadeHero";
 	
-	if(RegisterClass(&windowClass))
+	if(RegisterClassA(&windowClass))
 	{
 		HWND windowHandle = 
-			CreateWindowEx(
+			CreateWindowExA(
 				0,
 				windowClass.lpszClassName,
 				"HandMade",
@@ -114,14 +154,17 @@ WinMain(
 				0);
 		if(windowHandle)
 		{
+			running = true;
 			MSG message;
-			while(true)
+			while(running)
 			{
-				BOOL msgResult = GetMessage(&message, 0, 0, 0);
+				//GetMessageA can give you -1 as well if error occurs so exit
+				// when msgResult <= 0
+				BOOL msgResult = GetMessageA(&message, 0, 0, 0);
 				if(msgResult > 0)
 				{
 					TranslateMessage(&message);
-					DispatchMessage(&message);
+					DispatchMessageA(&message);
 				}
 				else 
 				{
