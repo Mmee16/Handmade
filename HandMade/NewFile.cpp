47460af81edaf795b123345d54b44b9876e15cfc
Sada <sadasiva.maddali123@gmail.com>
2025-12-31 15:10:24 +0530

    Animating Back buffer

--- HandMade/NewFile.cpp
+++ HandMade/NewFile.cpp
@@ -1,15 +1,44 @@
 #include <windows.h>
+#include <stdint.h>
+
 
 #define internal static
 #define global_var static
 
 
-
 global_var bool running;
+
 global_var void* bitmapMemory;
 global_var BITMAPINFO bitmapinfo;
-global_var HBITMAP bitmapHandle;
-global_var HDC bitmapDeviceContext;
+
+global_var int bitmapWidth;
+global_var int bitmapHeight;
+// bitmapMemory stores the info reg the color of each pixel
+// So the size of bitmapMemory = #(pixels) * 4 (- 4 = 3 rgb colors 3 bytes + 1 byte for padding)
+global_var int bytesPerPixel = 4;
+
+
+internal void renderBitmap(int xOffSet, int yOffSet)
+{
+	int width = bitmapWidth;
+	int height = bitmapHeight;
+	
+	uint8_t *row = (uint8_t *)bitmapMemory;
+	int pitch = width * bytesPerPixel;
+	for(int y=0; y<height; y++)
+	{
+		uint32_t *pixel = (uint32_t *)row;
+		for(int x=0; x<width; x++)
+		{
+			uint8_t blue = (x+xOffSet);
+			uint8_t green = (y+yOffSet);
+			// Memory 	BB GG RR xx
+			// Register xx RR GG BB - due to little endian
+			*pixel++ = ((green << 8) | blue);
+		}
+		row+=pitch;
+	}
+}
 
 //DIB - Device independent Bitmap
 // This function Creates a new bitmap when ever window size changes
@@ -21,36 +50,41 @@ fall back to first one
 internal void
 ResizeDIBSection(int width, int height)
 {
-	if(bitmapHandle)
+	if(bitmapMemory)
 	{
-		DeleteObject(bitmapHandle);
-	}
-	if(!bitmapDeviceContext)
-	{
-		bitmapDeviceContext = CreateCompatibleDC(0);
+		VirtualFree(bitmapMemory, 0, MEM_RELEASE);
 	}
 	
+	bitmapWidth = width;
+	bitmapHeight = height;
 	
 	bitmapinfo.bmiHeader.biSize = sizeof(bitmapinfo.bmiHeader);
-	bitmapinfo.bmiHeader.biWidth = width;
-	bitmapinfo.bmiHeader.biHeight = height;
+	bitmapinfo.bmiHeader.biWidth = bitmapWidth;
+	bitmapinfo.bmiHeader.biHeight = -bitmapHeight;
 	bitmapinfo.bmiHeader.biPlanes = 1;
 	bitmapinfo.bmiHeader.biBitCount = 32;
 	bitmapinfo.bmiHeader.biCompression = BI_RGB;
 	
-	bitmapHandle = CreateDIBSection(
-			0 , &bitmapinfo, DIB_RGB_COLORS,
-			&bitmapMemory, 0,0);
+	int bitmapMemorySize = (bitmapWidth * bitmapHeight) * bytesPerPixel;
 	
+	bitmapMemory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+	
+	renderBitmap(120, 0);
 }
 
 internal void 
-updateWindow(HDC deviceContext, int x, int y, int width, int height)
+updateWindow(HDC deviceContext, RECT *rect, int x, int y, int width, int height)
 {
+	
+	int windowWidth = rect->right - rect->left;
+	int windowHeight = rect->bottom - rect->top;
+	
 	StretchDIBits(
 		deviceContext,
-		x, y, width, height,
-		x, y, width, height,
+		// x, y, width, height,
+		// x, y, width, height,
+		0, 0, bitmapWidth, bitmapHeight,
+		0, 0, windowWidth, windowHeight,
 		bitmapMemory,
 		&bitmapinfo,
 		DIB_RGB_COLORS, SRCCOPY);
@@ -107,8 +141,9 @@ LRESULT MainCallBack(
 			int y = rect.top;
 			int width = rect.right - rect.left;
 			int height = rect.bottom - rect.top;
-			static DWORD color = BLACKNESS;
-			updateWindow(deviceContext, x, y, width, height);
+			RECT clientRect;
+			GetClientRect(windowHandle, &clientRect);
+			updateWindow(deviceContext, &clientRect, x, y, width, height);
 			EndPaint(windowHandle, &paint);
 
 			break;
@@ -155,23 +190,36 @@ WinMain(
 		if(windowHandle)
 		{
 			running = true;
-			MSG message;
+			MSG msg;
+			int xOffSet = 0;
+			int yOffSet = 0;
 			while(running)
 			{
 				//GetMessageA can give you -1 as well if error occurs so exit
 				// when msgResult <= 0
-				BOOL msgResult = GetMessageA(&message, 0, 0, 0);
-				if(msgResult > 0)
+				// BOOL msgResult = GetMessageA(&msg, 0, 0, 0);
+				// GetMessageA is ineffiecent call for windows switch to PeekMessageA
+				while(PeekMessageA(&msg, 0, 0, 0, PM_REMOVE))
 				{
-					TranslateMessage(&message);
-					DispatchMessageA(&message);
-				}
-				else 
-				{
-					// Log error
-					break;
+					if(msg.message == WM_QUIT)
+					{
+						running = false;
+					}
+					TranslateMessage(&msg);
+					DispatchMessageA(&msg);
 				}
 				
+				renderBitmap(xOffSet, yOffSet);
+				HDC deviceContext = GetDC(windowHandle);
+				RECT clientRect;
+				GetClientRect(windowHandle, &clientRect);
+				int windowWidth = clientRect.right - clientRect.left;
+				int windowHeight = clientRect.bottom - clientRect.top;
+				updateWindow(deviceContext, &clientRect, 0, 0, windowWidth, windowHeight);
+				ReleaseDC( windowHandle, deviceContext);
+				
+				// ++xOffSet;
+				++yOffSet;
 			}
 		}
 		else 
