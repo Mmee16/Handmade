7e145264250c3a0f8124dd913dd526e932044141
Sada <sadasiva.maddali123@gmail.com>
2026-01-01 14:56:37 +0530

    Build refactor

--- HandMade/build.bat
+++ HandMade/build.bat
@@ -1,12 +1,7 @@
 @echo off
-mkdir ..\build
-pushd ..\build
-cl > nul
-if %ERRORLEVEL% neq 0 (
-    echo "cl command failed, executing setup_build.bat"
-    call "..\set_debug.bat" > nul
-)
-cl -Zi ..\HandMade\NewFile.cpp User32.lib Gdi32.lib
+mkdir build
+pushd build
+cl -Zi ..\NewFile.cpp User32.lib Gdi32.lib
 if %ERRORLEVEL% neq 0 (
     echo "Build failed"
     Timeout /T 5
--- set_debug.bat
+++
@@ -1,2 +0,0 @@
-echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
